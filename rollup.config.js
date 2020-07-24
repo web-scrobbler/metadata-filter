0373101b8a6ad25b549035ec77428983a9c27ce3
alexesprit <alex.esprit@gmail.com>
2020-07-24 18:35:53 +0300

    Generate multiple dist files
    Generate ESM dist file.
    Generate minified and non-minified dist files.
    Generate sourcemaps.

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,38 @@
-import banner from 'rollup-plugin-banner';
+import banner from 'rollup-plugin-banner2';
 import { terser } from 'rollup-plugin-terser';
 
-const bannerText = `metadata-filter v<%= pkg.version %>
-(c) Web Scrobbler MetadataFilter Team
-Licensed under the MIT License (https://github.com/web-scrobbler/metadata-filter/blob/master/LICENSE.md)`;
+import { name, version } from './package.json';
+
+const bannerText = `/*
+ * ${name} v${version}
+ * (c) Web Scrobbler MetadataFilter Team
+ * Licensed under the MIT License
+ */
+`;
+
+function output(distFileName, format) {
+	const minify = distFileName.endsWith('.min');
+	const plugins = [];
+	if (minify) {
+		plugins.push(terser());
+	}
+
+	return {
+		format,
+		plugins,
+		file: `dist/${distFileName}.js`,
+		name: 'MetadataFilter',
+		sourcemap: true,
+	};
+}
 
 export default {
 	input: 'src/index.js',
-	output: {
-		file: 'dist/filter.js',
-		format: 'umd',
-		name: 'MetadataFilter',
-	},
-	plugins: [terser(), banner(bannerText)],
+	output: [
+		output('filter', 'umd'),
+		output('filter.min', 'umd'),
+		output('filter.esm', 'esm'),
+		output('filter.esm.min', 'esm'),
+	],
+	plugins: [banner(() => bannerText)],
 };
