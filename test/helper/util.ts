b0b7aada78c9c2923957227fe8221e20f97620c2
alexesprit <alex.esprit@gmail.com>
2021-01-09 00:56:51 +0300

    Remove unused helper function

--- test/helper/util.ts
+++ test/helper/util.ts
@@ -1,30 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-import { MetadataFilter, createFilter, FilterSet } from '../../src/filter';
-import type { FilterFunction } from '../../src/functions';
-
-/**
- * Return a MetadataFilter instance using the given filter function.
- * The filter function is applied to the given list of fields.
- *
- * @param filterFunc Filter function
- * @param fields List of fields
- *
- * @return MetadataFilter instance
- */
-export function createFilterFromFunction(
-	filterFunc: FilterFunction,
-	fields: string[]
-): MetadataFilter {
-	const filterSet = fields.reduce((filterSet, field) => {
-		filterSet[field] = filterFunc;
-		return filterSet;
-	}, {} as FilterSet);
-
-	return createFilter(filterSet);
-}
-
 /**
  * A dummy filter function that returns an input.
  *
